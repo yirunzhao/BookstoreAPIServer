55c17db92586a21676a8957d0ec04438bd75142d
yirunzhao <651844925@qq.com>
2020-03-22 22:41:17 +0800

    统一路径规范

--- APIServer/bookstore/storeauth/urls.py
+++ APIServer/bookstore/storeauth/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'storeauth'
 urlpatterns = [
-    path('login', views.login_view, name='login'),
-    path('register', views.register_view, name='register'),
-    path('modify', views.modify_user)
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('modify/', views.modify_user)
 ]
