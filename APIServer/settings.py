9ba479ce366c5db15641ecd42adf086d56b92396
yirunzhao <651844925@qq.com>
2020-03-22 20:48:56 +0800

    静态文件配置

--- APIServer/settings.py
+++ APIServer/settings.py
@@ -154,5 +154,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_dist')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
