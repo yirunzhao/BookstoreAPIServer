af153450754a88c2f4579ae1953b1123a0ff4036
yirunzhao <651844925@qq.com>
2020-03-22 20:46:44 +0800

    静态文件配置

--- APIServer/settings.py
+++ APIServer/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -29,7 +28,6 @@ ALLOWED_HOSTS = [
     'yrzhao.club',
     'www.yrzhao.club'
 ]
-
 
 # Application definition
 
@@ -104,7 +102,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'APIServer.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -138,7 +135,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -152,8 +148,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_dist')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
