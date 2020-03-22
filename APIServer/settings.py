f541c9dfe53847ec28a15f5a58039d23d38dfe7b
yirunzhao <651844925@qq.com>
2020-03-22 20:57:25 +0800

    static

--- APIServer/settings.py
+++ APIServer/settings.py
@@ -151,5 +151,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'APIServer/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
