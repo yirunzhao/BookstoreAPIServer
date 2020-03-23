d0711c516270bb29514157bfe4b31a109fc35162
yirunzhao <651844925@qq.com>
2020-03-23 21:30:42 +0800

    后台样式

--- APIServer/settings.py
+++ APIServer/settings.py
@@ -152,4 +152,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/ubuntu/bookstore/static'
